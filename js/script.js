767f8dcc77e922256e4b2e593e5e0bc19ac13bd5
KOndra96 <o.krechnak@seznam.cz>
2025-02-05 08:28:52 +0100

    popup added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 const footerContainer = document.querySelector('footer');
+
 const shareButton = document.querySelector('.share-button');
+const popup = document.querySelector('.popup');
 
 shareButton.addEventListener('click', (event) => {
     event.preventDefault();
-    
-    footerContainer.classList.toggle('clicked');
+
+    let screenWidth = Number(window.innerWidth);
+
+    // openPopUp(screenWidth);
+    popup.classList.toggle('hide');
+
+
+    console.log('clicked');
+    console.log('w: ' + window.innerWidth);
 });
+
+function openPopUp(screenWidth) {
+    if(screenWidth < 880) {
+        popup.classList.add('hide');
+
+
+    } else {
+        popup.classList.toggle('hide');
+    }
+}
